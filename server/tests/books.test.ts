bc685ba4c430d39deeac0cd45d632fd3b37fc626
Sarah Bizal <sarah.bizal@gmail.com>
2026-09-10 12:33:02 -0400

    Update books.test.ts with proper verification for GET, POST, PUT, and DELETE

--- server/tests/books.test.ts
+++ server/tests/books.test.ts
@@ -79,8 +79,56 @@ describe("PUT /books/:id", () => {
     expect(modifiedBook.body.readStatus).toBe(updateData.readStatus);
     expect(modifiedBook.body.title).toBe(createData.title);
   });
+
+  it("modifies all book fields", async () => {
+    const updateData = {
+      title: "Dracula",
+      author: "Bram Stoker",
+      genre: "Gothic Horror",
+      readStatus: "read",
+      coverImageUrl:
+        "https://en.wikipedia.org/wiki/File:Dracula-First-Edition-1897.jpg",
+    };
+
+    const modifiedBook = await request(app)
+      .put(`/books/${existingBookId}`)
+      .send(updateData);
+    expect(modifiedBook).toBeTruthy();
+    expect(modifiedBook.body.title).toBe(updateData.title);
+    expect(modifiedBook.body.author).toBe(updateData.author);
+    expect(modifiedBook.body.genre).toBe(updateData.genre);
+    expect(modifiedBook.body.readStatus).toBe(updateData.readStatus);
+    expect(modifiedBook.body.coverImageUrl).toBe(updateData.coverImageUrl);
+  });
 });
 
 describe("DELETE /books/:id", () => {
-  it("deletes individual books", async () => {});
+  let existingBookId: number | undefined;
+  const createData = {
+    title: "Moby Dick",
+    author: "Ernest Hemmingway",
+    genre: "Adventure",
+    readStatus: "want to read",
+    coverImageUrl:
+      "https://en.wikipedia.org/wiki/File:Moby-Dick_FE_title_page.jpg",
+  };
+
+  beforeAll(async () => {
+    const books = await request(app).get("/books");
+    expect(Array.isArray(books.body)).toBe(true);
+
+    const createdBook = await request(app).post("/books").send(createData);
+    existingBookId = createdBook.body.id;
+  });
+
+  it("deletes individual books", async () => {
+    const deleteBook = await request(app).delete(`/books/${existingBookId}`);
+    expect(deleteBook).toBeTruthy();
+
+    const checkBooks = await request(app).get("/books");
+    const stillExists = checkBooks.body.some(
+      (book: any) => book.id === existingBookId,
+    );
+    expect(stillExists).toBe(false);
+  });
 });
