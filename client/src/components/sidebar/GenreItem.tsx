143cb3071161047ebd1de90f301ab8bef51ec095
Sarah Bizal <sarah.bizal@gmail.com>
2026-09-20 15:15:56 -0400

    Refactor GenreList component and initialize GenreItem component

--- client/src/components/sidebar/GenreItem.tsx
+++ client/src/components/sidebar/GenreItem.tsx
@@ -1,3 +1,18 @@
-function GenreItem() {}
+interface GenreItemProps {
+  name: string;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+function GenreItem({ name, isSelected, onClick }: GenreItemProps) {
+  return (
+    <li
+      className={isSelected ? "genre-list-item active" : "genre-list-item"}
+      onClick={onClick}
+    >
+      {name}
+    </li>
+  );
+}
 
 export default GenreItem;
--- client/src/components/sidebar/GenreList.tsx
+++ client/src/components/sidebar/GenreList.tsx
@@ -1,3 +1,29 @@
-function GenreList() {}
+import { useState } from "react";
+import GenreItem from "./GenreItem";
+
+interface GenreListProps {
+  items: string[];
+  onSelectItem: (item: string) => void;
+}
+
+function GenreList({ items, onSelectItem }: GenreListProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <ul className="genre-list">
+      {items.map((item, index) => (
+        <GenreItem
+          key={item}
+          name={item}
+          isSelected={selectedIndex === index}
+          onClick={() => {
+            setSelectedIndex(index);
+            onSelectItem(item);
+          }}
+        />
+      ))}
+    </ul>
+  );
+}
 
 export default GenreList;
