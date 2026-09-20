0f65ca7ed6d3d5c345aec8ead90b4a02e4512caf
Sarah Bizal <sarah.bizal@gmail.com>
2026-09-20 14:34:06 -0400

    Initialize BookCard component

--- client/src/components/books/BookCard.tsx
+++ client/src/components/books/BookCard.tsx
@@ -1,3 +1,27 @@
-function BookCard() {}
+interface BookCardProps {
+  title: string;
+  author: string;
+  genre: string;
+  readStatus: string;
+  coverImageUrl: string | null;
+}
+
+function BookCard({
+  title,
+  author,
+  genre,
+  readStatus,
+  coverImageUrl,
+}: BookCardProps) {
+  return (
+    <div>
+      <img src={coverImageUrl ?? "placeholder.jpg"} alt={title} />
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <p>{genre}</p>
+      <p>{readStatus}</p>
+    </div>
+  );
+}
 
 export default BookCard;
