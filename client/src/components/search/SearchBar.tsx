76e5e84144f2a79955a7176ee68b01452cf927e4
Sarah Bizal <sarah.bizal@gmail.com>
2026-09-22 15:17:46 -0400

    Configure SearchBar component

--- client/src/components/search/SearchBar.tsx
+++ client/src/components/search/SearchBar.tsx
@@ -1,3 +1,33 @@
-function SearchBar() {}
+import { useState } from "react";
+
+interface SearchBarProps {
+  onSearch: (title: string) => void;
+}
+
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch(searchQuery);
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="search..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+      </form>
+    </div>
+  );
+}
 
 export default SearchBar;
